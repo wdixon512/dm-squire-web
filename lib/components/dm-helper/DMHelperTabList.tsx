60979deda14256d02272652e07210e2277acc70d
Will - Personal <isagny17@gmail.com>
2025-05-13 17:38:14 -0400

    refactor tab list/tab panels into smaller components

---
+++ lib/components/dm-helper/DMHelperTabList.tsx
@@ -0,0 +1,77 @@
+import { TabList, Tab, Icon, Image, Text } from '@chakra-ui/react';
+import React from 'react';
+import { FaUserCog } from 'react-icons/fa';
+
+export default function DMHelperTabList(props: { readOnlyRoom: boolean }) {
+  const { readOnlyRoom } = props;
+
+  return (
+    <>
+      <TabList
+        alignSelf="center"
+        border="2px solid"
+        justifyContent="center"
+        mb="4"
+        p={2}
+        w="fit-content"
+        bgColor="secondary.500"
+      >
+        {/* Combat Tab*/}
+        <Tab
+          _selected={{ color: 'white', bg: 'primary.200' }}
+          borderRadius="lg"
+          fontWeight="bold"
+          data-testid="combat-panel"
+        >
+          <Image src="/static/images/sword.png" alt="sword-icon" w="20px" h="20px" mr="1" />
+          <Text as="span" lineHeight="24px">
+            Combat
+          </Text>
+        </Tab>
+
+        {!readOnlyRoom && (
+          <>
+            {/* Heroes Tab*/}
+            <Tab
+              _selected={{ color: 'white', bg: 'primary.200' }}
+              borderRadius="lg"
+              fontWeight="bold"
+              data-testid="heroes-panel"
+            >
+              <Image src="/static/images/knight.png" alt="knight" w="20px" h="20px" mr="1" />
+              <Text as="span" lineHeight="24px">
+                Heroes
+              </Text>
+            </Tab>
+
+            {/* Invite Others Tab */}
+            <Tab
+              _selected={{ color: 'white', bg: 'primary.200' }}
+              borderRadius="lg"
+              fontWeight="bold"
+              data-testid="invite-others-panel"
+            >
+              <Image src="/static/images/join-party.png" alt="knight" w="20px" h="20px" mr="1" />
+              <Text as="span" lineHeight="24px">
+                Invite Others
+              </Text>
+            </Tab>
+          </>
+        )}
+
+        {/* Manage Tab */}
+        <Tab
+          _selected={{ color: 'white', bg: 'primary.200' }}
+          borderRadius="lg"
+          fontWeight="bold"
+          data-testid="user-room-settings-panel"
+        >
+          <Icon as={FaUserCog} w={6} h={6} mr={2} />
+          <Text as="span" lineHeight="24px">
+            Manage
+          </Text>
+        </Tab>
+      </TabList>
+    </>
+  );
+}
---
+++ lib/components/dm-helper/DMHelperTabPanels.tsx
@@ -0,0 +1,81 @@
+import { TabPanels, TabPanel, Flex, Box, Heading, Image } from '@chakra-ui/react';
+import React from 'react';
+import CombatManagementBar from './CombatManagementBar';
+import { EntityList } from './EntityList';
+import { HeroForm } from './HeroForm';
+import { HeroList } from './HeroList';
+import { InviteOthersForm } from './InviteOthersForm';
+import { MobForm } from './MobForm';
+import { MobQuickAdd } from './MobQuickAdd';
+import UserRoomSettingsComponent from './UserRoomSettingsComponent';
+
+export default function DMHelperTabPanels(props: { readOnlyRoom: boolean; combatStarted: boolean }) {
+  const { readOnlyRoom, combatStarted } = props;
+
+  return (
+    <TabPanels>
+      {/* Combat Tab Panel*/}
+      <TabPanel justifyItems={'center'}>
+        <Flex
+          direction={{ base: 'column', lg: 'row' }}
+          justifyContent="center"
+          gap="12"
+          w={readOnlyRoom ? { base: '100%', lg: '65%' } : '100%'}
+        >
+          {!readOnlyRoom && (
+            <Flex direction="column" gap="4" w={{ base: '100%', lg: '35%' }}>
+              <MobForm />
+              <MobQuickAdd />
+            </Flex>
+          )}
+          <Flex direction="column" gap="4" flex="1">
+            {readOnlyRoom && (
+              <Box w="100%" p={4} bg="blackAlpha.900" opacity=".95" borderWidth="1px" borderRadius="md" shadow="md">
+                {combatStarted && (
+                  <Heading variant="redSolid" data-testid="combat-started-heading" textAlign="center">
+                    <Box display="inline-flex" gap="4">
+                      <Image src="/static/images/sword.png" alt="sword-icon" w="2.25rem" h="2.25rem" mr="1" />
+                      Combat has started...
+                    </Box>
+                  </Heading>
+                )}
+                {!combatStarted && (
+                  <Heading data-testid="combat-ended-heading" textAlign="center">
+                    Combat has NOT started.
+                  </Heading>
+                )}
+              </Box>
+            )}
+
+            <EntityList />
+            {!readOnlyRoom && <CombatManagementBar />}
+          </Flex>
+        </Flex>
+      </TabPanel>
+
+      {/* Heroes Tab Panel*/}
+      {!readOnlyRoom && (
+        <TabPanel>
+          <Flex gap="4" justifyContent="center">
+            <HeroForm />
+            <HeroList />
+          </Flex>
+        </TabPanel>
+      )}
+
+      {/* Invite Others Tab Panel*/}
+      {!readOnlyRoom && (
+        <TabPanel>
+          <Flex gap="4" justifyContent="center">
+            <InviteOthersForm />
+          </Flex>
+        </TabPanel>
+      )}
+
+      {/* "Manage" Tab Panel */}
+      <TabPanel>
+        <UserRoomSettingsComponent />
+      </TabPanel>
+    </TabPanels>
+  );
+}
